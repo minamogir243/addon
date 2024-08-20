36cbe649f6ef6bab82a8cabf0b7e0017d3fd3f0c
minamogir243 <168736987+minamogir243@users.noreply.github.com>
2024-08-20 16:09:46 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// リダイレクト先のURLを指定
+const redirectUrl = "https://sites.google.com/view/addonsyoji/download/dl_money_2";  // リダイレクト先のURLを指定してください
+
+// 3秒後にリダイレクト
+setTimeout(() => {
+    window.location.href = redirectUrl;
+}, 3000);
